b9519d60306146befdc989f7751939604cb7270a
eren8204 <apskumar24@gmail.com>
2024-08-02 13:37:45 +0530

    commit adi

--- app/src/main/java/com/example/paymentapp/HomeFragment.java
+++ app/src/main/java/com/example/paymentapp/HomeFragment.java
@@ -8,11 +8,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ImageSlider imageSlider = view.findViewById(R.id.imageSlider);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
 
+        slideModels.add(new SlideModel(R.drawable.a, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.b, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.c, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.d, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.e, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.f, ScaleTypes.FIT));
+
+        imageSlider.setImageList(slideModels, ScaleTypes.FIT);
+        return  view;
     }
 }
