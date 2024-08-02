2f09a671fc548fd54089cc64eccb7a7626d4502a
eren8204 <apskumar24@gmail.com>
2024-08-02 16:52:47 +0530

    commit adi-2

--- app/src/main/java/com/example/paymentapp/HomeFragment.java
+++ app/src/main/java/com/example/paymentapp/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.paymentapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
@@ -15,10 +17,13 @@ import com.denzcoskun.imageslider.models.SlideModel;
 import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
+    @SuppressLint({"MissingInflatedId", "LocalSuppress"})
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        TextView marquee_text = view.findViewById(R.id.marquee_text);
+        marquee_text.setSelected(true);
         ImageSlider imageSlider = view.findViewById(R.id.imageSlider);
         ArrayList<SlideModel> slideModels = new ArrayList<>();
 
