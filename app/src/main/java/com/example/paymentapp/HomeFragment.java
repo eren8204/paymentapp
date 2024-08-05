cadd46f294c308ee44a3bd379eb4c3205344eda8
eren8204 <apskumar24@gmail.com>
2024-08-05 15:17:31 +0530

    commit adi-4

--- app/src/main/java/com/example/paymentapp/HomeFragment.java
+++ app/src/main/java/com/example/paymentapp/HomeFragment.java
@@ -5,28 +5,31 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
+    LinearLayout prepaid_mobile;
     @SuppressLint({"MissingInflatedId", "LocalSuppress"})
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
-
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         ImageSlider imageSlider = view.findViewById(R.id.imageSlider);
         TextView marquee_text = view.findViewById(R.id.marquee_text);
+        prepaid_mobile = view.findViewById(R.id.prepaid_mobile);
         marquee_text.setSelected(true);
         ArrayList<SlideModel> slideModels = new ArrayList<>();
 
@@ -38,6 +41,13 @@ public class HomeFragment extends Fragment {
         slideModels.add(new SlideModel(R.drawable.f, ScaleTypes.FIT));
 
         imageSlider.setImageList(slideModels, ScaleTypes.FIT);
+
+        prepaid_mobile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("click","Recharge");
+            }
+        });
         return  view;
     }
 }
